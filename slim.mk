67bdc20152561ee17319f060a7971209d5cdf407
pcexpress <pcexpress805@gmail.com>
2013-09-18 16:25:06 -0700

    Update slim.mk

--- slim.mk
+++ slim.mk
@@ -22,18 +22,18 @@ PRODUCT_RELEASE_NAME := GEEB
 $(call inherit-product-if-exists, device/lge/geeb/full_geeb.mk)
 
 # gsm
-$(call inherit-product, vendor/slim/config/common_gsm.mk)
+$(call inherit-product, vendor/slim/config/gsm.mk)
 
 # languages
 PRODUCT_LOCALES := en_US de_DE zh_CN zh_TW cs_CZ nl_BE nl_NL en_AU en_GB en_CA en_NZ en_SG fr_BE fr_CA fr_FR fr_CH de_AT de_LI de_CH it_IT it_CH ja_JP ko_KR pl_PL ru_RU es_ES ar_EG ar_IL bg_BG ca_ES hr_HR da_DK en_IN en_IE en_ZA fi_FI el_GR iw_IL hi_IN hu_HU in_ID lv_LV lt_LT nb_NO pt_BR pt_PT ro_RO sr_RS sk_SK sl_SI es_US sv_SE tl_PH th_TH tr_TR uk_UA vi_VN
 
 # phone
-$(call inherit-product, vendor/slim/config/common_phone.mk)
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 # products
 PRODUCT_DEVICE := geeb
 PRODUCT_BRAND := LGE
-PRODUCT_NAME := carbon_geeb
+PRODUCT_NAME := slim_geeb
 PRODUCT_MODEL := Optimus G
 PRODUCT_MANUFACTURER := LGE
 PRODUCT_PROPERTY_OVERRIDES += ro.buildzipid=slim.geeb.$(shell date +%m%d%y).$(shell date +%H%M%S)
