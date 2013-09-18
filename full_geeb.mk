17642b8d16f15d1bf29073b20a6fa023a39e86a8
pcexpress <pcexpress805@gmail.com>
2013-09-18 16:25:50 -0700

    Update full_geeb.mk

--- full_geeb.mk
+++ full_geeb.mk
@@ -32,4 +32,4 @@ PRODUCT_RESTRICT_VENDOR_FILES := false
 
 # Inherit from hardware-specific part of the product configuration
 $(call inherit-product, device/lge/geeb/device.mk)
-$(call inherit-product-if-exists, vendor/lge/gee/gee-vendor.mk)
+$(call inherit-product-if-exists, vendor/lge/geeb/geeb-vendor.mk)
