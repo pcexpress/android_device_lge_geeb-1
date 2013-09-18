fe67cf8e2374248c51fea91ce60c1995dabd1706
pcexpress <pcexpress805@gmail.com>
2013-09-18 15:35:06 -0700

    Update carbon.mk

--- carbon.mk
+++ carbon.mk
@@ -22,13 +22,13 @@ PRODUCT_RELEASE_NAME := GEEB
 $(call inherit-product-if-exists, device/lge/geeb/full_geeb.mk)
 
 # gsm
-$(call inherit-product, vendor/carbon/config/common_gsm.mk)
+$(call inherit-product, vendor/slim/config/common_gsm.mk)
 
 # languages
 PRODUCT_LOCALES := en_US de_DE zh_CN zh_TW cs_CZ nl_BE nl_NL en_AU en_GB en_CA en_NZ en_SG fr_BE fr_CA fr_FR fr_CH de_AT de_LI de_CH it_IT it_CH ja_JP ko_KR pl_PL ru_RU es_ES ar_EG ar_IL bg_BG ca_ES hr_HR da_DK en_IN en_IE en_ZA fi_FI el_GR iw_IL hi_IN hu_HU in_ID lv_LV lt_LT nb_NO pt_BR pt_PT ro_RO sr_RS sk_SK sl_SI es_US sv_SE tl_PH th_TH tr_TR uk_UA vi_VN
 
 # phone
-$(call inherit-product, vendor/carbon/config/common_phone.mk)
+$(call inherit-product, vendor/slim/config/common_phone.mk)
 
 # products
 PRODUCT_DEVICE := geeb
@@ -36,13 +36,13 @@ PRODUCT_BRAND := LGE
 PRODUCT_NAME := carbon_geeb
 PRODUCT_MODEL := Optimus G
 PRODUCT_MANUFACTURER := LGE
-PRODUCT_PROPERTY_OVERRIDES += ro.buildzipid=carbon.geeb.$(shell date +%m%d%y).$(shell date +%H%M%S)
+PRODUCT_PROPERTY_OVERRIDES += ro.buildzipid=slim.geeb.$(shell date +%m%d%y).$(shell date +%H%M%S)
 
 # overrides
 PRODUCT_BUILD_PROP_OVERRIDES += \
     BUILD_NUMBER=737497 \
-    PRODUCT_NAME=gee \
+    PRODUCT_NAME=geeb \
     TARGET_BUILD_TYPE=user \
     BUILD_VERSION_TAGS=release-keys \
-    PRIVATE_BUILD_DESC="gee-user 4.3 JWR66V 737497 release-keys" \
+    PRIVATE_BUILD_DESC="gee-user 4.3 JSS15 573038 release-keys" \
     BUILD_FINGERPRINT="lge/gee/geeb:4.3/JWR66V/737497:user/release-keys"
