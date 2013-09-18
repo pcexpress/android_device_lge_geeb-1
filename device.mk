30d3d1cc5844a096d5eceacf52be324147f21564
pcexpress <pcexpress805@gmail.com>
2013-09-18 16:27:33 -0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -284,6 +284,6 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
 # This is the mako-specific audio package
-$(call inherit-product, frameworks/base/data/sounds/AudioPackage10.mk)
+#$(call inherit-product, frameworks/base/data/sounds/AudioPackage10.mk)
 
 $(call inherit-product, hardware/qcom/msm8960/msm8960.mk)
